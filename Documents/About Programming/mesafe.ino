48a576ce7484a045ef389eeaf83776b1e7764647
berkboyraz <56213602+berkboyraz@users.noreply.github.com>
2020-02-11 20:53:41 +0300

    Arduino Code for Ultrasound Sensor

---
+++ Documents/About Programming/mesafe.ino
@@ -0,0 +1,27 @@
+int trigPin = 2; 
+int echoPin = 7; 
+
+long sure;
+long uzaklik;
+
+void setup(){
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin,INPUT); 
+  Serial.begin(9600); 
+}
+void loop()
+{
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);   
+  sure = pulseIn(echoPin, HIGH); 
+  uzaklik= sure /29.1/2;             
+  if(uzaklik > 200)
+    uzaklik = 200;
+  Serial.print("Uzaklik ");  
+  Serial.print(uzaklik); 
+  Serial.println(" CM ");  
+  delay(100); 
+}
