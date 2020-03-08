75737c290b786d99b08a8ba94074af6447835e61
onur-akdeniz <56254910+onur-akdeniz@users.noreply.github.com>
2020-03-08 20:45:36 +0300

    Add files via upload

---
+++ Documents/About Programming/read_write_further_study_3.py
@@ -0,0 +1,27 @@
+# 	load and display an image with Matplotlib
+from matplotlib import image
+from matplotlib import pyplot
+import numpy as np
+import os
+from PIL import Image
+
+i=0
+y= np.zeros((256,256,3))
+
+#image = image.imread('eee.jpg')
+#
+#while i<256:
+#	x = image[i]
+#	np.savetxt(os.path.join("text_" + str(i)+".txt"), x, fmt='%s')
+#	i=i+1
+#
+#i=0
+
+while i<256:
+	y[i] = np.loadtxt(os.path.join("text_" + str(i)+".txt"), dtype=np.object) 
+	i=i+1
+
+y = y.astype(np.uint8)
+
+img = Image.fromarray(y)
+img.save('test.jpg')
